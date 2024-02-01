6efd3a2891eddd536e09c4659aa470dd659afcb6
Ashwary Tripathi <105623462+ashwarytripath@users.noreply.github.com>
2024-02-01 21:49:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from sklearn.externals import joblib
 import pandas as pd
-import joblib
 
 app = Flask(__name__)
-
-# Load the trained model
 model = joblib.load('linear_regression_model.joblib')
 
 @app.route('/')
@@ -13,14 +11,9 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract data from the form
     inflation = float(request.form['inflation'])
     unemployment = float(request.form['unemployment'])
-
-    # Make prediction
     prediction = model.predict([[inflation, unemployment]])[0]
-
-    # Return prediction as JSON
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
